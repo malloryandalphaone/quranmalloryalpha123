972419a8fc70bf71f90d591105def0ee3289c5f8
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2018-08-25 02:27:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,6 +33,29 @@ message.gulid.member(message.author).voiceChannel.join()
 .catch(console.error);
 });
 
-
+    const adminprefix = "quran-";
+const devs = ['380307890235506698','ID OWNER OF BOT'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setgame')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
+}); 
 
 client.login(process.env.BOT_TOKEN);
